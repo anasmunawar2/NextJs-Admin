b7f97c96e5d3b098f34113e3b3ef4bb3eecba3ba
anasmunawar2 <anasmunawar2@gmail.com>
2024-06-27 23:14:16 +0500

    user-product page added

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import styles from "../../ui/dashboard/products/products.module.css";
+import Search from "./../../ui/dashboard/search/search";
+import Link from "next/link";
+import Image from "next/image";
+import Pagination from "./../../ui/dashboard/pagination/pagination";
 
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product..." />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                Iphone
+              </div>
+            </td>
+            <td>Desc</td>
+            <td>$500</td>
+            <td>15.05.2024</td>
+            <td>75</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default ProductsPage;
+
